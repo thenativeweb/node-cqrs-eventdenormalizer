93262fd254dffbcc5078107b7587c35e7c809288
Adriano Raiano <adriano@raiano.ch>
2014-09-10 10:15:02 +0200

    initialization data for collection

--- synopsis/person/collection.js
+++ synopsis/person/collection.js
@@ -1,4 +1,10 @@
 // if exports is an array, it will be the same like loading multiple files...
 module.exports = require('cqrs-eventdenormalizer').defineCollection({
   name: 'person' // optional, default is folder name
+},
+
+// optionally, define some initialization data for new view models...
+{
+  emails: ['default@mycomp.org'],
+  phoneNumbers: []
 });
