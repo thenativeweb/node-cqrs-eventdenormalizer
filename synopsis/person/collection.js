9cd719931aa841a3537ed087293dc9ef77cd09b0
Adriano Raiano <adriano@raiano.ch>
2014-09-12 14:31:14 +0200

    introduced collection

--- synopsis/person/collection.js
+++ synopsis/person/collection.js
@@ -1,6 +1,7 @@
 // if exports is an array, it will be the same like loading multiple files...
 module.exports = require('cqrs-eventdenormalizer').defineCollection({
   name: 'person' // optional, default is folder name
+//  defaultPayload: 'payload'
 },
 
 // optionally, define some initialization data for new view models...
