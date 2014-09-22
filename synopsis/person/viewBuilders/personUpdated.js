9e6b8885b800ccdcac19706918fa53cc1be7986d
Adriano Raiano <adriano@raiano.ch>
2014-09-22 13:05:08 +0200

    removed old stuff and synopsis

--- synopsis/person/viewBuilders/personUpdated.js
+++
@@ -1,9 +0,0 @@
-// if exports is an array, it will be the same like loading multiple files...
-module.exports = require('cqrs-eventdenormalizer').defineViewBuilder({
-  name: 'personUpdated', // optional, default is file name without extension, if name is '' it will handle all events that matches
-  aggregate: 'employee', // optional
-  context: 'hr',         // optional
-  // version: 2, // optional, default is 0
-  id: 'payload.id', // if not defined or not found it will generate a new viewmodel with new id
-  payload: 'payload' // optional, if not defined it will pass the whole event...
-}, 'update');
